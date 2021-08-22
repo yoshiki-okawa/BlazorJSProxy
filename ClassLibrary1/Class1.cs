991f4e77aa698d52816f09a1fc8b6b3328a7f993
Yoshiki Okawa <yoshiki.okawa@gmail.com>
2021-08-22 16:00:32 +1200

    Initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+	[AttributeUsage(AttributeTargets.Interface)]
+	public class JSTargetAttribute : Attribute
+	{
+		public string ConstructorFunction { get; set; }
+		public string GlobalVariable { get; set; }
+	}
+
+	[JSTarget(ConstructorFunction = "RTCPeerConnection")]
+	public interface IRTCPeerConnection : IAsyncDisposable
+	{
+		ValueTask<string> ConnectionState { get; }
+	}
+
+	public class MouseEvent
+	{
+		public int OffsetX { get; set; }
+		public int OffsetY { get; set; }
+	}
+
+	[JSTarget(GlobalVariable = "window")]
+	public interface IWindow : IAsyncDisposable
+	{
+		ValueTask<T> Eval2<T>(string code, params object[] arguments);
+		Func<MouseEvent, Task> Onclick { set; }
+	}
+
+	public class SyncBlazorJSProxyCallback<T>
+	{
+		private readonly Action<T> callback;
+
+		public SyncBlazorJSProxyCallback(Action<T> callback)
+		{
+			this.callback = callback;
+		}
+
+		[JSInvokable]
+		public async Task InvokeAsync(T evt)
+		{
+			callback.Invoke(evt);
+			await Task.CompletedTask;
+		}
+	}
+
+	public class AsyncBlazorJSProxyCallback<T>
+	{
+		private readonly Func<T, Task> callback;
+
+		public AsyncBlazorJSProxyCallback(Func<T, Task> callback)
+		{
+			this.callback = callback;
+		}
+
+		[JSInvokable]
+		public async Task InvokeAsync(T evt)
+		{
+			await callback.Invoke(evt);
+		}
+	}
+
+	static class BlazorJSProxyInitializer
+	{
+		public static MethodInfo JsRuntimeInvokeAsyncMethodInfo = typeof(IJSRuntime).GetMethod(nameof(IJSObjectReference.InvokeAsync), new Type[] { typeof(string), typeof(object[]) });
+		public static MethodInfo JsObjectInvokeAsyncMethodInfo = typeof(IJSObjectReference).GetMethod(nameof(IJSObjectReference.InvokeAsync), new Type[] { typeof(string), typeof(object[]) });
+		private static bool init = false;
+		public static async Task InitializeAsync(IJSRuntime jsRuntime)
+		{
+			if (!init)
+			{
+				await jsRuntime.InvokeVoidAsync("eval", "window.eval2 = function() { return new Function(arguments[0]).apply(null, arguments[1]); }");
+				await jsRuntime.InvokeVoidAsync("eval2", "window.getProperty = function(x, y) { return x[y]; }");
+				await jsRuntime.InvokeVoidAsync("eval2", "window.setProperty = function(x, y, z) { return x[y] = z; }");
+				await jsRuntime.InvokeVoidAsync("eval2", @"window.cloneObjWithFilter = function (obj, filter)
+{
+	if (Object(obj) !== obj)
+		return obj;
+	else if (Array.isArray(obj))
+		return obj.reduce((previousValue, currentValue) =>
+		{
+			if (filter(undefined, currentValue))
+				return previousValue.push(cloneObjWithFilter(currentValue, filter));
+			return previousValue;
+		}, []);
+
+	let newObj = {};
+	for (let k in obj)
+	{
+		let v = obj[k];
+		if (filter(k, v))
+			newObj[k] = cloneObjWithFilter(obj[k], filter);
+	}
+	return newObj;
+}");
+				await jsRuntime.InvokeVoidAsync("eval2", @"window.prepareObjectForCallback = (obj) =>
+{
+					console.log(cloneObjWithFilter);
+					return cloneObjWithFilter(obj, (k, v) => v !== window && typeof v !== 'function' && !(v instanceof Element) && !(v instanceof HTMLDocument));
+				}
+				");
+				await jsRuntime.InvokeVoidAsync("eval2", "window.setEventHandler = function(x, y, z) { console.log(z);return x[y] = function() { z.invokeMethodAsync('InvokeAsync', prepareObjectForCallback(arguments[0])) }; }");
+			}
+			init = true;
+		}
+	}
+
+	public class BlazorJSProxy<TInterface> : DispatchProxy where TInterface : IAsyncDisposable
+	{
+		private static readonly Type interfaceType = typeof(TInterface);
+		private static readonly string constructorFunction = interfaceType.GetCustomAttribute<JSTargetAttribute>()?.ConstructorFunction ?? Regex.Replace(interfaceType.Name, "^I", String.Empty);
+		private static readonly string globalVariable = interfaceType.GetCustomAttribute<JSTargetAttribute>()?.GlobalVariable;
+		private IJSRuntime jsRuntime;
+		private IJSObjectReference target;
+
+		public static async Task<TInterface> CreateAsync(IJSRuntime jsRuntime, params object[] parameters)
+		{
+			await BlazorJSProxyInitializer.InitializeAsync(jsRuntime);
+			TInterface proxy = Create<TInterface, BlazorJSProxy<TInterface>>();
+			var Proxy = proxy as BlazorJSProxy<TInterface>;
+			Proxy.jsRuntime = jsRuntime;
+			if(!String.IsNullOrWhiteSpace(globalVariable))
+				Proxy.target = await jsRuntime.InvokeAsync<IJSObjectReference>("eval2", $"return {globalVariable};");
+			else
+				Proxy.target = await jsRuntime.InvokeAsync<IJSObjectReference>("eval2", $"return new (Function.prototype.bind.apply({constructorFunction}, arguments));", parameters);
+			return proxy;
+		}
+
+		protected override object Invoke(MethodInfo targetMethod, object[] args)
+		{
+			if (targetMethod.DeclaringType == typeof(IAsyncDisposable))
+				return target.DisposeAsync();
+
+			if (targetMethod.Name.StartsWith("get_") || targetMethod.Name.StartsWith("set_"))
+			{
+				var propertyName = targetMethod.Name[4..];
+				propertyName = Char.ToLower(propertyName[0]) + propertyName[1..];
+				if (args.Length > 0 && args[0] is Delegate)
+				{
+					var arguments = args[0].GetType().GetGenericArguments();
+					var callbackType = arguments[0];
+					var isAsync = arguments.Length > 1;
+					var eventHandler = (isAsync ? typeof(AsyncBlazorJSProxyCallback<>) : typeof(SyncBlazorJSProxyCallback<>)).MakeGenericType(callbackType).GetConstructors()[0].Invoke(new object[] { args[0] });
+					var objectRef = typeof(DotNetObjectReference).GetMethod("Create").MakeGenericMethod(eventHandler.GetType()).Invoke(null, new object[] { eventHandler });
+					return jsRuntime.InvokeVoidAsync("setEventHandler", target, propertyName, objectRef);
+				}
+				if (targetMethod.Name.StartsWith("get_"))
+				{
+					MethodInfo invokeAsyncMethodInfo = BlazorJSProxyInitializer.JsRuntimeInvokeAsyncMethodInfo.MakeGenericMethod(targetMethod.ReturnType.GenericTypeArguments[0]);
+					return invokeAsyncMethodInfo.Invoke(jsRuntime, new object[] { "getProperty", new object[] { target, propertyName } });
+				}
+				else
+					return jsRuntime.InvokeVoidAsync("setProperty", target, propertyName, args[0]);
+			}
+
+			string methodName = Char.ToLower(targetMethod.Name[0]) + targetMethod.Name[1..];
+			if (targetMethod.ReturnType.IsGenericType)
+				return BlazorJSProxyInitializer.JsObjectInvokeAsyncMethodInfo.MakeGenericMethod(targetMethod.ReturnType.GenericTypeArguments[0]).Invoke(target, new object[] { methodName, args });
+			return target.InvokeVoidAsync(methodName, args);
+		}
+	}
+
+	public class Class1
+	{
+		private IJSRuntime jsRuntime;
+		public Class1(IJSRuntime jsRuntime)
+		{
+			this.jsRuntime = jsRuntime;
+		}
+
+		public async Task Test()
+		{
+			var window = await BlazorJSProxy<IWindow>.CreateAsync(jsRuntime);
+			var aaa = await window.Eval2<string>("return 'aaa';");
+			window.Onclick = async evt => {
+				var a = evt.OffsetX;
+				var b = evt.OffsetY;
+			};
+
+			var peer = await BlazorJSProxy<IRTCPeerConnection>.CreateAsync(jsRuntime);
+			var a = await peer.ConnectionState;
+			await peer.DisposeAsync();
+			/*var a = await jsRuntime.InvokeAsync<IJSObjectReference>("eval", "new RTCPeerConnection()");
+			var b = await jsRuntime.InvokeAsync<string>("getProperty", a, "connectionState");
+			var t = b.GetType().FullName;
+			var c = await a.InvokeAsync<IJSObjectReference>("getConfiguration");
+			var d = await a.InvokeAsync<object>("getConfiguration");*/
+		}
+	}
+}
---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary1;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorJSProxy.Pages
+{
+	public partial class Counter
+	{
+		[Inject]
+		private Class1 Class1 { get; set; }
+
+		protected override async Task OnInitializedAsync()
+		{
+			await base.OnInitializedAsync();
+			await Class1.Test();
+		}
+	}
+}
